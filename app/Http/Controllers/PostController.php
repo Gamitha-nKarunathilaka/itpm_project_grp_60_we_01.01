265a2de26798628d1bef55e688295df8283bb7a8
OsadaThilanVimukthi <99235607+OsadaThilanVimukthi@users.noreply.github.com>
2023-04-03 16:27:22 +0300

    View Details frontend

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PostController extends Controller
+{
+    public function getAllPost(){
+        $_POST =DB::table('admins')->get();
+        return view('welcome',compact('POST'));
+    }
+}
