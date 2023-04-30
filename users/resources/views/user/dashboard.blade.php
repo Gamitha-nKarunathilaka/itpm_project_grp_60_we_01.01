af72f9d62e57418e56ebca8807314768910ccf80
IT20648336 <IT20648336@my.sliit.lk>
2023-04-30 22:04:39 +0530

    ADD CHANGES

--- users/resources/views/user/dashboard.blade.php
+++ users/resources/views/user/dashboard.blade.php
@@ -1,1 +1,30 @@
-<h1>user</h1>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                        <h1>{{ __('Welcome') }}, {{ Auth::user()->name }}</h1>
+                        <h2>{{ __('Your Notifications') }}</h2>
+
+                        <ul>
+                            @forelse ($notifications as $notification)
+                                <li>
+                                    {{ $notification->data['message'] }}
+                                    <br>
+                                    <small>{{ $notification->created_at->diffForHumans() }}</small>
+                                </li>
+                            @empty
+                                <li>{{ __('No notifications.') }}</li>
+                            @endforelse
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
