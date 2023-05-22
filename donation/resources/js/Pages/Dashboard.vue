3e8eefa1a86b727d7da420bafdf7caccfd1b6912
gamithanarada <100194030+Gamitha-nKarunathilaka@users.noreply.github.com>
2023-05-22 16:00:59 +0530

    full project

---
+++ donation/resources/js/Pages/Dashboard.vue
@@ -0,0 +1,44 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+
+            
+            </h2>
+            <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="products" rel="stylesheet" >
+    
+    <title>Button</title>
+  
+</head>
+<body>
+    <div class="container">
+        <h1 style="text-align:center;">Html Link Button With action attribute</h1>
+        <form action="products">
+            <button class="btn btn-danger btn-lg">Click</button>
+        </form>
+    </div>
+</body>
+           
+
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+
+                    
+                    <Welcome />
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
---
+++ donation/resources/views/home.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ViewPoint</title>
+    <link href="{{ asset('/../css/home.css') }}" rel="stylesheet">
+</head>
+<body>
+   <!-- namvigation  -->
+   <nav>
+    
+      <div class="logo">
+        <p>ViewPoint</p>
+      </div>
+
+        <ul>
+            <li><a href="#"></a>Home</li>
+            <li><a href="#"></a>Programme</li>
+            <li><a href="#"></a>Donate</li>
+            <li><a href="#"></a>About</li>
+            <button class="button">Log In</button>
+
+        </ul>
+        
+   </nav>
+   <div class="slider">
+        <div class="slider-1">
+            <img src="./img/1.jpg" alt="">
+            <div class="text">
+            <h1>Donate your own </h1>
+            <span></span>
+            <p>Your gift to Green City Market builds a more resilient, more sustainable, 
+                and more accessible local food system in Chicago. When you make a tax-deductible donation, 
+                you lift up small-scale, sustainable farmers, educate the community about where food comes from and why 
+                it matters, and fight for food security for all Chicagoans</p>
+            </div>
+            
+        </div>
+      
+</body>
+</html>
